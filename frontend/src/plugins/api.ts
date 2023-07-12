fd49fc54d9fd39c25bebac7c003ccfd97b68060f
Boris <boris@sellfer.com>
2023-07-12 14:54:17 +0300

    api plugin finalization

--- frontend/src/plugins/api.ts
+++ frontend/src/plugins/api.ts
@@ -4,14 +4,60 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:3000",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use((request): any => {
-    console.log('request:', request)
     const authStore = useAuthStore();
 
-    request.headers.Authorization = `Bearer ${authStore.accessToken}`
+    if (authStore.accessToken && request.url !== '/auth/refresh') {
+        request.headers.Authorization = `Bearer ${authStore.accessToken}`;
+    }
+
+    return request;
 });
+
+axiosInstance.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    async (err) => {
+        const authStore = useAuthStore();
+
+        const originalConfig = err.config;
+
+        if (originalConfig.url !== "/auth/refresh" && err.response) {
+            // Access Token was expired
+            if (err.response.status === 401 && !originalConfig._retry) {
+                originalConfig._retry = true;
+
+                try {
+                    const rs = await axiosInstance.post("/auth/refresh");
+
+                    const { access_token } = rs.data;
+
+                    console.log('da');
+                    authStore.setToken(access_token);
+
+                    return axiosInstance(originalConfig);
+                } catch (_error) {
+                    authStore.setToken("");
+
+                    return Promise.reject(_error);
+                }
+            }
+        }
+
+        if (originalConfig.url === "/auth/refresh" && err.response) {
+            authStore.setToken("");
+        }
+
+        return Promise.reject(err);
+    }
+);
 
 const injectionKey = Symbol('http');
 
