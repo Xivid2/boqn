ab81f2cba5c8084be04704d25ebe854443283e6a
Boris <boris@sellfer.com>
2023-07-12 13:25:21 +0300

    add auth service for requests

---
+++ frontend/src/services/auth.service.ts
@@ -0,0 +1,27 @@
+export default class AuthService {
+    constructor(useHttp) {
+        this.http = useHttp();
+    }
+
+    async login(email: string, password: string) {
+        try {
+            const { data } = await this.http.post('/auth/login', {
+                email, password,
+            });
+
+            return { data };
+        } catch (error) {
+            return { error };
+        }
+    }
+
+    async refreshTokens() {
+        try {
+            const { data } = await this.http.post('/auth/refresh');
+
+            return { data };
+        } catch (error) {
+            return { error };
+        }
+    }
+}
