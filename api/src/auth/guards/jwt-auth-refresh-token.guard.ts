09f15bd463f5690a6b3b7ce15a55774c646b51e0
Boris <boris@sellfer.com>
2023-07-12 10:31:27 +0300

    fix jwt refresh token guard

--- api/src/auth/guards/jwt-auth-refresh-token.guard.ts
+++ api/src/auth/guards/jwt-auth-refresh-token.guard.ts
@@ -1,5 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtRefreshTokenGuard extends AuthGuard('jwt-refresh') {}
+export class JwtRefreshTokenGuard extends AuthGuard('jwt-refresh') {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest<Request>();
+
+        let refreshToken;
+
+        refreshToken = request.cookies['jwt'];
+
+        if (!refreshToken) {
+            refreshToken = request.body?.refreshToken;
+        }
+
+        request.headers['authorization'] = `Bearer ${refreshToken}`;
+
+        return super.canActivate(context);
+    }
+}
