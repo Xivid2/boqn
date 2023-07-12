b6646b50e3a3733d56e82729bb9ee78ca6ff04dc
Boris <boris@sellfer.com>
2023-07-12 13:25:03 +0300

    handle routing with authentication

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,28 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth.store';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/sign-in',
-      name: 'sign-in',
-      component: () => import('../views/SignInView.vue')
-    }
-  ]
+history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+            meta: {
+                requiresAuth: false
+            }
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/AboutView.vue'),
+            meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/SignInView.vue'),
+            meta: {
+                requiresAuth: false,
+                redirectIfAuthenticated: true,
+            }
+        }
+    ]
 })
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+    
+    const isAuthenticated = !!authStore.accessToken;
+
+    if (to.meta.requiresAuth && !isAuthenticated) {
+        next('/login');
+    } else {
+        next();
+    }
+});
 
 export default router
