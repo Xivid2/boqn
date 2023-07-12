a07c09b77032a2ddbc1f7b92be52d92240f6dad9
Boris <boris@sellfer.com>
2023-07-12 14:53:39 +0300

    auth.store fixes

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -3,12 +3,10 @@ import { defineStore } from 'pinia';
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         accessToken: '',
-        refreshToken: '',
     }),
     actions: {
-        setTokens(accessToken: string, refreshToken: string) {
+        setToken(accessToken: string, refreshToken: string) {
             this.accessToken = accessToken;
-            this.refreshToken = refreshToken;
         },
     },
 });
