1e1935b7a9c81cfa9526d0dfa0b79b017c882597
Boris <boris@sellfer.com>
2023-07-13 12:37:32 +0300

    create-user.dto add validation

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,18 +1,25 @@
-import { IsNotEmpty, IsEmail } from 'class-validator';
+import { IsNotEmpty, IsEmail, Equals, MinLength, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
     @IsNotEmpty()
     userRoleId: number
 
     @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(50)
     firstName: string
 
     @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(50)
     lastName: string
 
     @IsEmail()
+    @MaxLength(255)
     email: string
 
     @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(50)
     password: string
 };
