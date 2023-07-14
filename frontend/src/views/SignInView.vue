ca00cea0ec2be85f3f909f05d5c45b74bd2df5f0
Boris <boris@sellfer.com>
2023-07-14 11:11:58 +0300

    remove old files

--- frontend/src/views/SignInView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div style="display: flex; justify-content: center;">
-        <Login />
-    </div>
-</template>
-
-<script lang="ts" setup>
-import Login from "../components/signin/Login.vue";
-</script>
