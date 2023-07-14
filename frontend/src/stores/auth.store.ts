c9d8518f4e28d210cc5e87ec3cd212de8e289f30
Boris <boris@sellfer.com>
2023-07-14 11:17:23 +0300

    improve auth store

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -4,9 +4,17 @@ export const useAuthStore = defineStore('auth', {
     state: () => ({
         accessToken: '',
     }),
+    getters: {
+        isAuthenticated(): boolean {
+            return !!this.accessToken;
+        }
+    },
     actions: {
         setToken(accessToken: string) {
             this.accessToken = accessToken;
         },
+        setUnauthenticated() {
+            this.accessToken = '';
+        }
     },
 });
