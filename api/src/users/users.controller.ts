33df5b82d1168917bb0872d3d067c373c2109324
Boris <boris@sellfer.com>
2023-07-18 15:01:19 +0300

    remove unused

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Req, Res, Get, Post, Body, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -9,7 +8,6 @@ export class UsersController {
         private userService: UsersService
     ) {}
 
-    // @UseGuards(LocalAuthGuard)
     @Post('')
     async create(@Body() createUserDto: CreateUserDto) {
         return this.userService.create(createUserDto);
