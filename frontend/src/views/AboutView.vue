2a50d781f872f42b68c3b05b5bfe593e714934ee
Boris <boris@sellfer.com>
2023-08-02 13:09:42 +0300

    finalize about us page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,68 +1,73 @@
 <template>
     <div class="wrapper">
-        <card>
-            <v-row>
-                <v-col>
-                    <img class="image" src="../assets/images/man.png" />
-                </v-col>
-    
-                <v-col>
-                    <p style="margin-top:20%">Names: Boris Milkov Marinov</p>
-    
-                    <p class="mt-4">Profession: kitenasda</p>
-    
-                    <p class="mt-4">Education: Somehwere that has finished university</p>
-                </v-col>
-            </v-row>
+        <card class="py-8 px-8">
+            <div class="about-us-container">
+                <div class="about-us-container__header">
+                    <div class="about-us-container__image">
+                        <img class="image" src="../assets/images/man.png" />
+                    </div>
 
-            <v-row>
-                <v-col>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aperiam cumque sapiente modi exercitationem quia, architecto ex vitae rem ipsam quod repellat voluptas dolor quos dignissimos eius recusandae reiciendis in.
-                    Maiores dolore nemo ad assumenda laborum culpa architecto eum quo at beatae ducimus ipsa quis autem dicta labore nostrum explicabo sint magnam, perferendis repellat similique laudantium. Amet perferendis esse fuga!
-                    Incidunt, culpa quaerat, reprehenderit eaque magni quibusdam iste doloremque enim recusandae a commodi maxime, ea consequuntur atque in architecto. Quam voluptas corrupti sapiente perspiciatis error ad similique cupiditate consequuntur veniam.
-                    Tempore, numquam dicta aliquid voluptates cum velit id officia rerum voluptatem maxime autem tenetur quasi sint, asperiores, dignissimos pariatur impedit maiores itaque magnam ipsum. Quasi numquam suscipit ipsam beatae perspiciatis!
-                    Magnam ex dolorem earum. Laudantium impedit consequuntur fugiat omnis earum eveniet commodi neque, odit, iste fugit provident saepe! Ea quaerat doloribus at voluptatum ad, numquam unde rerum doloremque reiciendis corrupti.
-                    Voluptatibus, neque ipsum ipsam, quisquam quibusdam, impedit sapiente iusto a debitis nemo quae sint aliquam vero nisi maxime sit. Vero provident dignissimos iure. A, at ducimus harum cum ratione hic.
-                    Accusamus tempore minima optio necessitatibus nam soluta voluptas inventore laborum sapiente doloremque ducimus asperiores expedita consequatur facilis, deleniti reiciendis exercitationem molestiae temporibus vitae mollitia! Neque, quis! Distinctio perspiciatis corrupti quidem.
-                    Ratione in qui sequi, at quibusdam dolore tempore tempora vitae, ipsam quidem facilis distinctio ab similique odio nesciunt esse excepturi deserunt. Quos ex non cumque laboriosam ipsa tenetur, deserunt sunt.
-                    Quibusdam et, saepe, quos ex dolores nulla amet blanditiis, praesentium odio labore quasi! Cum, iusto sapiente! Minima, adipisci dolorum eaque nam tempora, quibusdam, eos quos non neque delectus corporis obcaecati.
-                    Et ipsa non iure nemo reiciendis consequatur accusantium tenetur eos laboriosam minus adipisci porro perspiciatis eum fugiat odio, itaque unde nostrum eius explicabo iste, repellendus quasi corporis nobis dicta! Asperiores.
-                </v-col>
-            </v-row>
+                    <div class="about-us-container__info">
+                        <p>Boris Milkov Marinov</p>
+    
+                        <p>Profession: kitenasda</p>
+        
+                        <p>Education: Somehwere that has finished university</p>
+                    </div>
+                </div>
+
+                <div class="about-us-container__body mt-4">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aperiam cumque sapiente modi exercitationem quia, architecto ex vitae rem ipsam quod repellat voluptas dolor quos dignissimos eius recusandae reiciendis in. Maiores dolore nemo ad assumenda laborum culpa architecto eum quo at beatae ducimus ipsa quis autem dicta labore nostrum explicabo sint magnam, perferendis repellat similique laudantium. Amet perferendis esse fuga! Incidunt, culpa quaerat, reprehenderit eaque magni quibusdam iste doloremque enim recusandae a commodi maxime, ea consequuntur atque in architecto. Quam voluptas corrupti sapiente perspiciatis error ad similique cupiditate consequuntur veniam. Tempore, numquam dicta aliquid voluptates cum velit id officia rerum voluptatem maxime autem tenetur quasi sint, asperiores, dignissimos pariatur impedit maiores itaque magnam ipsum. Quasi numquam suscipit ipsam beatae perspiciatis! Magnam ex dolorem earum. Laudantium impedit consequuntur fugiat omnis earum eveniet commodi neque, odit, iste fugit provident saepe! Ea quaerat doloribus at voluptatum ad, numquam unde rerum doloremque reiciendis corrupti. Voluptatibus, neque ipsum ipsam, quisquam quibusdam, impedit sapiente iusto a debitis nemo quae sint aliquam vero nisi maxime sit. Vero provident dignissimos iure. A, at ducimus harum cum ratione hic. Accusamus tempore minima optio necessitatibus nam soluta voluptas inventore laborum sapiente doloremque ducimus asperiores expedita consequatur facilis, deleniti reiciendis exercitationem molestiae temporibus vitae mollitia! Neque, quis! Distinctio perspiciatis corrupti quidem. Ratione in qui sequi, at quibusdam dolore tempore tempora vitae, ipsam quidem facilis distinctio ab similique odio nesciunt esse excepturi deserunt. Quos ex non cumque laboriosam ipsa tenetur, deserunt sunt. Quibusdam et, saepe, quos ex dolores nulla amet blanditiis, praesentium odio labore quasi! Cum, iusto sapiente! Minima, adipisci dolorum eaque nam tempora, quibusdam, eos quos non neque delectus corporis obcaecati. Et ipsa non iure nemo reiciendis consequatur accusantium tenetur eos laboriosam minus adipisci porro perspiciatis eum fugiat odio, itaque unde nostrum eius explicabo iste, repellendus quasi corporis nobis dicta! Asperiores.
+                    </p>
+                </div>
+            </div>
         </card>
 
-        <card class="my-8">
-            <v-row>
-                <v-col>
-                    <p style="margin-top:20%">Names: Boris Milkov Marinov</p>
+        <card class="py-8 px-8 my-8">
+            <div class="about-us-container">
+                <div class="about-us-container__header">
+                    <div class="about-us-container__image">
+                        <img class="image" src="../assets/images/man.png" />
+                    </div>
+
+                    <div class="about-us-container__info">
+                        <p>Boris Milkov Marinov</p>
     
-                    <p class="mt-4">Profession: kitenasda</p>
-    
-                    <p class="mt-4">Education: Somehwere that has finished university</p>
-                </v-col>
-                
-                <v-col>
-                    <img class="image" src="../assets/images/man.png" />
-                </v-col>
-            </v-row>
-    
-            <v-row>
-                <v-col>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aperiam cumque sapiente modi exercitationem quia, architecto ex vitae rem ipsam quod repellat voluptas dolor quos dignissimos eius recusandae reiciendis in.
-                    Maiores dolore nemo ad assumenda laborum culpa architecto eum quo at beatae ducimus ipsa quis autem dicta labore nostrum explicabo sint magnam, perferendis repellat similique laudantium. Amet perferendis esse fuga!
-                    Incidunt, culpa quaerat, reprehenderit eaque magni quibusdam iste doloremque enim recusandae a commodi maxime, ea consequuntur atque in architecto. Quam voluptas corrupti sapiente perspiciatis error ad similique cupiditate consequuntur veniam.
-                    Tempore, numquam dicta aliquid voluptates cum velit id officia rerum voluptatem maxime autem tenetur quasi sint, asperiores, dignissimos pariatur impedit maiores itaque magnam ipsum. Quasi numquam suscipit ipsam beatae perspiciatis!
-                    Magnam ex dolorem earum. Laudantium impedit consequuntur fugiat omnis earum eveniet commodi neque, odit, iste fugit provident saepe! Ea quaerat doloribus at voluptatum ad, numquam unde rerum doloremque reiciendis corrupti.
-                    Voluptatibus, neque ipsum ipsam, quisquam quibusdam, impedit sapiente iusto a debitis nemo quae sint aliquam vero nisi maxime sit. Vero provident dignissimos iure. A, at ducimus harum cum ratione hic.
-                    Accusamus tempore minima optio necessitatibus nam soluta voluptas inventore laborum sapiente doloremque ducimus asperiores expedita consequatur facilis, deleniti reiciendis exercitationem molestiae temporibus vitae mollitia! Neque, quis! Distinctio perspiciatis corrupti quidem.
-                    Ratione in qui sequi, at quibusdam dolore tempore tempora vitae, ipsam quidem facilis distinctio ab similique odio nesciunt esse excepturi deserunt. Quos ex non cumque laboriosam ipsa tenetur, deserunt sunt.
-                    Quibusdam et, saepe, quos ex dolores nulla amet blanditiis, praesentium odio labore quasi! Cum, iusto sapiente! Minima, adipisci dolorum eaque nam tempora, quibusdam, eos quos non neque delectus corporis obcaecati.
-                    Et ipsa non iure nemo reiciendis consequatur accusantium tenetur eos laboriosam minus adipisci porro perspiciatis eum fugiat odio, itaque unde nostrum eius explicabo iste, repellendus quasi corporis nobis dicta! Asperiores.
-                </v-col>
-            </v-row>
+                        <p>Profession: kitenasda</p>
+        
+                        <p>Education: Somehwere that has finished university</p>
+                    </div>
+                </div>
+
+                <div class="about-us-container__body mt-4">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aperiam cumque sapiente modi exercitationem quia, architecto ex vitae rem ipsam quod repellat voluptas dolor quos dignissimos eius recusandae reiciendis in. Maiores dolore nemo ad assumenda laborum culpa architecto eum quo at beatae ducimus ipsa quis autem dicta labore nostrum explicabo sint magnam, perferendis repellat similique laudantium. Amet perferendis esse fuga! Incidunt, culpa quaerat, reprehenderit eaque magni quibusdam iste doloremque enim recusandae a commodi maxime, ea consequuntur atque in architecto. Quam voluptas corrupti sapiente perspiciatis error ad similique cupiditate consequuntur veniam. Tempore, numquam dicta aliquid voluptates cum velit id officia rerum voluptatem maxime autem tenetur quasi sint, asperiores, dignissimos pariatur impedit maiores itaque magnam ipsum. Quasi numquam suscipit ipsam beatae perspiciatis! Magnam ex dolorem earum. Laudantium impedit consequuntur fugiat omnis earum eveniet commodi neque, odit, iste fugit provident saepe! Ea quaerat doloribus at voluptatum ad, numquam unde rerum doloremque reiciendis corrupti. Voluptatibus, neque ipsum ipsam, quisquam quibusdam, impedit sapiente iusto a debitis nemo quae sint aliquam vero nisi maxime sit. Vero provident dignissimos iure. A, at ducimus harum cum ratione hic. Accusamus tempore minima optio necessitatibus nam soluta voluptas inventore laborum sapiente doloremque ducimus asperiores expedita consequatur facilis, deleniti reiciendis exercitationem molestiae temporibus vitae mollitia! Neque, quis! Distinctio perspiciatis corrupti quidem. Ratione in qui sequi, at quibusdam dolore tempore tempora vitae, ipsam quidem facilis distinctio ab similique odio nesciunt esse excepturi deserunt. Quos ex non cumque laboriosam ipsa tenetur, deserunt sunt. Quibusdam et, saepe, quos ex dolores nulla amet blanditiis, praesentium odio labore quasi! Cum, iusto sapiente! Minima, adipisci dolorum eaque nam tempora, quibusdam, eos quos non neque delectus corporis obcaecati. Et ipsa non iure nemo reiciendis consequatur accusantium tenetur eos laboriosam minus adipisci porro perspiciatis eum fugiat odio, itaque unde nostrum eius explicabo iste, repellendus quasi corporis nobis dicta! Asperiores.
+                    </p>
+                </div>
+            </div>
         </card>
     </div>
 </template>
 
-<style>
+<style scoped>
+.about-us-container__header {
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    grid-template-columns: repeat(2, 50%);
+    gap: 1rem;
+}
+
+.about-us-container__info {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+
+.container__image {
+    justify-self: center;
+}
+
 </style>
