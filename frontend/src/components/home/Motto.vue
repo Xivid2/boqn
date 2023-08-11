432b6d0dfe278526c64ce0534a63ebf91fa69506
Boris <boris@sellfer.com>
2023-08-11 14:59:01 +0300

    just deploy

---
+++ frontend/src/components/home/Motto.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="motto text-center">
+        <h1>
+            Ние можем
+        </h1>
+
+        <p class="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe illo sunt dolore molestias minima, quas necessitatibus adipisci assumenda ea ab alias deleniti delectus. Facilis numquam sit eligendi rem et!
+        Molestiae voluptate ut distinctio voluptatem corporis voluptas similique repellat recusandae quasi praesentium nihil provident mollitia soluta inventore voluptatibus assumenda consequatur eligendi odio, nulla illum veritatis aliquid blanditiis. Repellat, sunt eos.
+        Animi nam, sint distinctio incidunt rem suscipit assumenda ducimus vero ipsum excepturi praesentium modi sed quam iste saepe magnam similique quos autem laboriosam qui voluptatibus. Aliquam amet alias quis consequatur.
+        Fugiat, vero? Cupiditate, culpa? Est nobis non odit ut possimus doloribus eveniet excepturi asperiores delectus, voluptatem, ex ratione pariatur quisquam dignissimos fuga alias odio laudantium officiis suscipit! Nisi, dolorem sapiente.
+        Sed veritatis aut, autem tempore optio magnam odio perferendis exercitationem eaque id, suscipit totam. Nisi deserunt quidem natus numquam aut fugiat dolor cupiditate molestias corporis, suscipit eos libero iure quas.
+        </p>
+    </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
+<template>
+    <div class="wide-wrapper mb-8">
+        <Carousel />
 
+        <Motto class="mt-16" />
+
+        <Video class="mt-8" />
+
+        <Footer />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import Carousel from '@/components/home/Carousel.vue';
+import Motto from '@/components/home/Motto.vue';
+import Video from '@/components/home/Video.vue';
 </script>
 
-<template>
-  <main>
-    home
-  </main>
-</template>
+<style scoped>
+</style>
