8ec125f07ba149e07a565662f8a4b1e58e3139ee
Boris <boris@sellfer.com>
2023-08-21 16:30:38 +0300

    add translations for mobileMenu

--- frontend/src/constants/global.ts
+++ frontend/src/constants/global.ts
@@ -1,1 +1,4 @@
 export const CurrencySuffix = "лв.";
+export const ContactPhoneFull = "+359887107087";
+export const ContactPhone = "0887107087";
+export const ContactAddress = "гр. Казанлък, ул. Хината 14";
