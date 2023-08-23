cc8aa9a0d33199c9b51fc41fbad3bf912d75d482
Boris <boris@sellfer.com>
2023-08-23 16:20:41 +0300

    add t

--- frontend/src/constants/AppointmentsTranslations.ts
+++ frontend/src/constants/AppointmentsTranslations.ts
@@ -1,4 +1,5 @@
 export const TAppointmentsCannotGetForPeriod = "Възникна проблем при опита за взимане на заетите часове";
+export const TAppointmentsCannotGetForStaffForWeek = "Възникна проблем при опита за взимане на заетите часове за служител по седмица";
 export const TAppointmentsYouNeedToChooseValue = "Трябва да изберете час";
 export const TAppointmentsCannotCreate = "Възникна проблем при опита за запис на час";
 export const TAppointmentsCreatedSuccessfully = "Успешно записахте час";
