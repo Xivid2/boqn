1d21471e97082a1230da38e12ae94182d2c82568
Boris <boris@sellfer.com>
2023-08-23 09:27:24 +0300

    don't get them, something bugs

--- frontend/src/views/services/ServicesView.vue
+++ frontend/src/views/services/ServicesView.vue
@@ -30,8 +30,4 @@
 <script lang="ts" setup>
 import { RouterLink, RouterView } from 'vue-router';
 import * as translations from '@/constants/ServicesTranslations';
-import { useServicesStore } from '@/stores/services.store';
-const servicesStore = useServicesStore();
-
-servicesStore.getAll();
 </script>
